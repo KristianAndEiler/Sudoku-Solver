f444fb0fa1373bb9899b0d93e6c69603168cc6ab
Eiler Poulsen <microhive@gmail.com>
2014-09-15 13:56:33 +0100

    Project Structure
    Launch!

---
+++ SudokuSolver/SudokuSolver.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SudokuSolver.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
